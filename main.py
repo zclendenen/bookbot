3a3a138e751a10fa62ce5c281ec5fccb619de9bd
Zach Clendenen <zclendenen@yahoo.com>
2025-01-12 23:55:11 +0000

    added individual character dictionary

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_word_count(text)
-    print(word_count)
+    char_count = individual_character_count(text)
+    print(char_count)
+    
 
 
 def get_book_text(path):
@@ -16,6 +18,20 @@ def get_word_count(text):
     for word in words:
         word_count += 1
     return word_count
+
+
+def individual_character_count(text):
+    character_count = {}
+    for character in text:
+        lower = character.lower()
+        if lower not in character_count:
+            character_count[lower] = 1
+        else:
+            character_count[lower] += 1
+    return character_count
+            
+
+
 
 
 main()
