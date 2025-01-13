a2de8c572bad8490156d0d2da3b1f56d65e5c97a
Zach Clendenen <zclendenen@yahoo.com>
2025-01-13 06:39:30 +0000

    added report function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     char_count = individual_character_count(text)
-    book_report(word_count, char_count)
+    book_report(book_path, word_count, char_count)
     
 
 
@@ -30,18 +30,22 @@ def individual_character_count(text):
             character_count[lower] += 1
     return character_count
             
-def book_report(word_count, char_count):
+def book_report(book_path, word_count, char_count):
     char_count_list = []
     def sort_on(dict):
-        return dict["count"]
+        return dict["num"]
     for char, count in char_count.items():
         if char.isalpha():
             char_count_list.append({
                 "char": char,
-                "count": count
+                "num": count
             })
     char_count_list.sort(reverse=True, key=sort_on)
-    print(char_count_list)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    for char_dict in char_count_list:
+        print(f"The {char_dict['char']} character was found {char_dict['num']} times")
+    print("--- End report ---")
 
 
 
